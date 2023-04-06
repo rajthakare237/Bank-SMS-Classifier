78c791af367aeda3d6481cf301df45d4f98df378
rajthakare237 <87536278+rajthakare237@users.noreply.github.com>
2023-04-06 13:26:44 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pickle
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title("Bank SMS Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    vector_input = tfidf.transform([input_sms])
+
+    result = model.predict(vector_input)[0]
+
+    if result == 1:
+        st.header("Bank SMS")
+    else:
+        st.header("Normal SMS")
